4f2fa225044e46ebee0509cc4642097c69c3da1b
sevg78 <e.sokolov@yandex.ru>
2022-11-23 00:38:05 +0400

    221122

---
+++ js/xxx.js
@@ -0,0 +1,21 @@
+var modal = document.getElementById("login")
+var btn = document.getElementById("btn1")
+var clos = document.getElementById("btn2")
+
+btn.onclick = function () {
+    modal.style.opacity = 1;
+    modal.style.transition = '0.7s opacity';
+    modal.style.pointerEvents = 'auto';
+}
+clos.onclick = function () {
+    modal.style.opacity = 0;
+    modal.style.transition = '0.7s opacity';
+    modal.style.pointerEvents = 'none';
+}
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.opacity = 0;
+        modal.style.transition = '0.7s opacity';
+        modal.style.pointerEvents = 'none';
+    }
+}
